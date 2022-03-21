dbf222f534b6259f11a9c3dbc4c0af40189aa123
Rosa <ygao112@jhu.edu>
2022-03-20 21:16:30 -0400

    adding styles

--- src/components/CreateMeeting.tsx
+++ src/components/CreateMeeting.tsx
@@ -1,20 +1,26 @@
-import React from "react"
-import { useState } from "react";
+import React, { useState } from "react";
 import { Grid, FormControl, TextField} from '@material-ui/core'
 import { useAppDispatch } from '../app/hooks'
 import { createMeeting } from "../pages/meetingNameSlice";
 import { useNavigate } from 'react-router-dom'
 import '../styles/button.css'
 
-const CreateMeeting = (props) => {
+const styles = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "200px",
+}
+
+const CreateMeeting = () => {
     const dispatch = useAppDispatch();
     const navigate = useNavigate();
     const [name, setName] = useState("")
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        dispatch(createMeeting(name))
-        navigate("/create")
+        dispatch(createMeeting(name));
+        navigate("/create");
       };
 
     // Update name every time user changes text in text field
@@ -23,31 +29,37 @@ const CreateMeeting = (props) => {
       };
 
     return (
-        <>
-        <Grid container
-        spacing={0}
-        direction="column"
-        alignItems="center"
-        justify="center"
-        style={{ minHeight: "100vh" }}>
-            
-        <form>
-        <FormControl fullWidth>
-          <TextField
+      <>
+      <Grid container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: "100vh"}}>
+
+        <FormControl style={{height:"40px"}}>
+          <TextField fullWidth
+            margin="dense"
+            style={styles}
             placeholder="Name the Meeting:"
             aria-label="Name the Meeting:"
             value={name}
             onChange={updateName}
           />
-        </FormControl>
-        <button type="submit" className="btn btn--add btn__text" onClick={handleSubmit}>
+          </FormControl>
+          
+          <button 
+            type="submit" 
+            className="btn btn--add btn__text" 
+            onClick={handleSubmit}>
             Create
-        </button>
-      </form>
-
-        </Grid>
+          </button>
         
-        </>
+        
+        
+
+      </Grid> 
+      </>
         
     )
 }
